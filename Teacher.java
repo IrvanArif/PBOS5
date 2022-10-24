b00e715cc2d662d20e148dc8b567dc152bbe768a
Irvan Arif <114166811+IrvanArif@users.noreply.github.com>
2022-10-24 22:45:58 +0700

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,16 @@
+package PBOS5;
+
+public class Teacher {
+
+    public Teacher(String name, String address) {
+    }
+
+    public boolean removeCourse(String course) {
+        return false;
+    }
+
+    public boolean addCourse(String course) {
+        return false;
+    }
+
+}
